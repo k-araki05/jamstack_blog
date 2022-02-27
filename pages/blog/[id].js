4a0ea2efc02247faea27a8e211defc511ac81bc6
araki <k-araki@tribeat.com>
2022-02-27 09:06:22 +0900

    test

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -13,7 +13,7 @@ export default function BlogId({ blog }) {
         />
         <>
         <Image
-        src="/assets/34340c486c204f439c1cb3bfb2b17901/e938979160744b4d831d82000ed291da/honda.JPG"
+        src={blog.image_01.url}
         alt="pic"
         width={blog.image_01.height}
         height={blog.image_01.width}
